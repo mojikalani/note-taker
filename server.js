7c539a66dc08423f20a0ee60aa4cd872ca557b7f
kalanimojica <mojica.kalani@gmail.com>
2021-10-21 19:17:57 -0700

    Updated server js file

--- server.js
+++ server.js
@@ -1,16 +1,51 @@
 const express = require("express");
-const fs = require("fs");
+const app = express();
 const path = require("path");
-const database = require("./db/db")
+const fs = require("fs");
+const uuid = require("uuid");
 
-var app = express();
-var PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
-app.use(express.static('public'));
 
+
+//Could be in its own file and folder to have less code in server.js
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT}`)
-);
+
+//Routes (Could be in its own file)
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+    fs.readFile(path.join(__dirname, "./db/db.json"), (err, data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data);
+        res.json(notes);
+    })
+});
+
+app.post("/api/notes", function(req, res) {
+    fs.readFile(path.join(__dirname, "./db/db.json"), (err, data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data);
+        const newNote = req.body;
+        newNote.id = uuid.v4();
+        notes.push(newNote);
+
+        const createNote = JSON.stringify(notes);
+        fs.writeFile(path.join(__dirname, "./db/db.json"), createNote, (err) =>{
+            if (err) throw err;
+        });
+        res.json(newNote);
+    });
+});
+
+app.listen(PORT, function() {
+    console.log(`App listening at http://localhost:${PORT}`);
+});
